34f730c267496a7d5b42453f85946cb3f71f4bb6
David Eriksson <the.dvudd@gmail.com>
2024-05-29 15:11:34 +0000

    fix: fixes typos and rephrases some sections

--- app_pages/page_prediction.py
+++ app_pages/page_prediction.py
@@ -16,13 +16,15 @@ def page_prediction_body():
     model = load_pkl_file(f"outputs/ml_pipeline/{version}/pipeline.pkl")
     features = (pd.read_csv(f"outputs/ml_pipeline/{version}/x_train.csv").columns.to_list())
 
-    st.warning(
-        f"* A page displaying the 4 houses' attributes and their respective predicted sale price. It should display a message informing the summed predicted price for all 4 inherited houses. You should add interactive input widgets that allow a user to provide real-time house data to predict the sale price."
-    )
-
     # Inherited House Price Predictions
     st.write("### Sale Price Prediction Interface - Inherited Houses")
     st.text("")
+
+    st.info(
+        # Copied from README file - "Business Requirements" section
+        f"* Business requirement 2 - The client is interested in predicting the house sale prices from\n"
+        f"her 4 inherited houses, and any other house in Ames, Iowa."
+    )
 
     df_inherited = load_inherited_data()
 
