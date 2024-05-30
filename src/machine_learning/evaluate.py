488eba7beec861ee3c1a3243cd7a59b6a9c3177d
David Eriksson <the.dvudd@gmail.com>
2024-05-30 03:39:13 +0000

    docs: adds docstrings to functions in evaluate.py

--- src/machine_learning/evaluate.py
+++ src/machine_learning/evaluate.py
@@ -4,6 +4,11 @@ from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 
 
 def performance(x_train, y_train, x_test, y_test, pipeline):
+    """
+    This function evaluates the performance of a trained machine learning pipeline
+    on both the training and test datasets, displaying the evaluation metrics in
+    a Streamlit app.
+    """
     st.write("Model Evaluation \n")
     st.write("* Train Set")
     evaluation(x_train, y_train, pipeline)
@@ -12,6 +17,11 @@ def performance(x_train, y_train, x_test, y_test, pipeline):
 
 
 def evaluation(x, y, pipeline):
+    """
+    This function predicts the target values using the provided pipeline and
+    input samples, then calculates and displays the R2 score, mean absolute
+    error, mean squared error, and root mean squared error using Streamlit.
+    """
     prediction = pipeline.predict(x)
     st.write('R2 Score:', r2_score(y, prediction).round(2))
     st.write('Mean Absolute Error:', mean_absolute_error(
