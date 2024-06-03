2d5fa1825774b4360a4e046f7d6cda8ba4e885a0
David Eriksson <david.eriksson@lundkroken.se>
2024-06-03 15:32:24 +0200

    feat: adds hypothesis page

--- app_pages/page_hypothesis.py
+++ app_pages/page_hypothesis.py
@@ -4,6 +4,17 @@ def page_hypothesis_body():
 
     st.write("### Project Hypothesis and Validation")
 
-    st.warning(
-        f"* A page indicating your project hypothesis(es) and how you validated it across the project."
+    st.success(
+        f"### Quality"
+        f"Validated through a correlation study."
+        f"* Findings indicate that the overall quality and kitchen quality of a house significantly"
+        f"influence the sale price."
+        f"### Size"
+        f"Validated through a correlation study."
+        f"* Results show that larger above-ground living areas and garage sizes positively impact the"
+        f"sale price."
+        f"### Age"
+        f"Validated through a correlation study."
+        f"* The property's age has an effect on its sale price. This indicates that the construction"
+        f"date of both the house and garage has an impact on the sale price of the property."
     )
