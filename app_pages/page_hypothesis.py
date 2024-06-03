a49482fd24ddafb7d701ac7904a56dddf5926bf4
David Eriksson <the.dvudd@gmail.com>
2024-06-03 13:42:12 +0000

    fix: fixes layout and makes hypothesis page PEP8 compliant

--- app_pages/page_hypothesis.py
+++ app_pages/page_hypothesis.py
@@ -1,20 +1,26 @@
 import streamlit as st
 
+
 def page_hypothesis_body():
 
     st.write("### Project Hypothesis and Validation")
 
+    st.write("#### Quality")
     st.success(
-        f"### Quality\n"
         f"Validated through a correlation study.\n"
-        f"* Findings indicate that the overall quality and kitchen quality of a house significantly"
-        f"influence the sale price.\n"
-        f"### Size\n"
+        f"* Findings indicate that the overall quality and kitchen quality of"
+        f" a house significantly influence the sale price.\n"
+    )
+    st.write("#### Size")
+    st.success(
         f"Validated through a correlation study.\n"
-        f"* Results show that larger above-ground living areas and garage sizes positively impact the"
-        f"sale price.\n"
-        f"### Age\n"
+        f"* Results show that larger above-ground living areas and garage"
+        f" sizes positively impact the sale price.\n"
+    )
+    st.write("#### Age")
+    st.success(
         f"Validated through a correlation study.\n"
-        f"* The property's age has an effect on its sale price. This indicates that the construction"
-        f"date of both the house and garage has an impact on the sale price of the property.\n"
+        f"* The property's age has an effect on its sale price. This indicates"
+        f" that the construction date of both the house and garage has an"
+        f" impact on the sale price of the property.\n"
     )
